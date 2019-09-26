f573c5ef463e1c720a34db48009f75fe2813ba54
L200180056 <55400856+ariwibowoo@users.noreply.github.com>
2019-09-26 21:41:29 +0700

    Add files via upload

---
+++ MobilDemo.java
@@ -0,0 +1,11 @@
+public class MobilDemo{
+	public static void main(String[] args){
+		Mobil mobil = new Mobil();
+		mobil.beriNama("Pajero");
+		mobil.beriJenis("Sport");
+		mobil.beriWarna("Silver");
+		mobil.besarCc(2000);
+		mobil.hargaJual(500);
+		mobil.infoMobil();
+	}
+}
